a4bf8d67fe9281f36122a2f349d77b84c01de5f9
Anwesh43 <anweshthecool0@gmail.com>
2018-06-04 02:41:00 +0530

    adding mountainview class

--- mountainview/src/main/java/com/anwesh/uiprojects/mountainview/MountainView.kt
+++ mountainview/src/main/java/com/anwesh/uiprojects/mountainview/MountainView.kt
@@ -4,3 +4,25 @@ package com.anwesh.uiprojects.mountainview
  * Created by anweshmishra on 04/06/18.
  */
 
+import android.view.View
+import android.content.Context
+import android.graphics.*
+import android.view.MotionEvent
+
+class MountainView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
