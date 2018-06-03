fb9b1a27583d000509c2a4a4d822f62c45f1e7ac
Anwesh43 <anweshthecool0@gmail.com>
2018-06-04 03:30:07 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinmountainview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinmountainview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinmountainview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.mountainview.MountainView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MountainView.create(this)
     }
 }
